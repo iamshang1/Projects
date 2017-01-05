b81708f076c826867e86202af96b4da3ab7ebf88
iamshang1 <emailshang@gmail.com>
2017-01-05 08:16:10 -0500

    added additional comments, resized lstm units and embeddings

--- Advanced_ML/Language_Modeling/model2.py
+++ Advanced_ML/Language_Modeling/model2.py
@@ -6,7 +6,12 @@ import gensim
 import string
 import re
 import sys
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
+#increase recursion limit for lstm
 sys.setrecursionlimit(10000)
 
 #load hgttg corpus
@@ -26,12 +31,12 @@ dataset = dataset.translate(None, string.punctuation).lower().split()
 
 #train word2vec
 print "training word2vec"
-w2v = gensim.models.Word2Vec(sentences, size=100, min_count=1, workers=4, iter=10, sample=0)
+w2v = gensim.models.Word2Vec(sentences, size=200, min_count=1, workers=4, iter=10, sample=0)
 w2v.init_sims(replace=True)
 
 #save all word embeddings to matrix
 print "saving word vectors to matrix"
-vocab = np.empty((len(w2v.vocab),100))
+vocab = np.empty((len(w2v.vocab),200))
 word2id = {}
 for key,val in w2v.vocab.iteritems():
     idx = val.__dict__['index']
@@ -46,25 +51,28 @@ vocab = scaler.fit_transform(vocab) * 0.5
 #lstm architecture
 class lstm(object):
     def __init__(self,vocab_size):
+    
+        #nn architecture
         self.input = T.matrix()
-        self.Wi = theano.shared(self.ortho_weight(128)[:100,:],borrow=True)
-        self.Wf = theano.shared(self.ortho_weight(128)[:100,:],borrow=True)
-        self.Wc = theano.shared(self.ortho_weight(128)[:100,:],borrow=True)
-        self.Wo = theano.shared(self.ortho_weight(128)[:100,:],borrow=True)
-        self.Ui = theano.shared(self.ortho_weight(128),borrow=True)
-        self.Uf = theano.shared(self.ortho_weight(128),borrow=True)
-        self.Uc = theano.shared(self.ortho_weight(128),borrow=True)
-        self.Uo = theano.shared(self.ortho_weight(128),borrow=True)
-        self.bi = theano.shared(np.asarray(np.zeros(128),dtype=theano.config.floatX),borrow=True)
-        self.bf = theano.shared(np.asarray(np.zeros(128),dtype=theano.config.floatX),borrow=True)
-        self.bc = theano.shared(np.asarray(np.zeros(128),dtype=theano.config.floatX),borrow=True)
-        self.bo = theano.shared(np.asarray(np.zeros(128),dtype=theano.config.floatX),borrow=True)
-        self.C0 = theano.shared(np.asarray(np.zeros(128),dtype=theano.config.floatX),borrow=True)
-        self.h0 = theano.shared(np.asarray(np.zeros(128),dtype=theano.config.floatX),borrow=True)
-        self.W2 = theano.shared(np.random.randn(128,vocab_size).astype(theano.config.floatX)*np.sqrt(1.5/vocab_size),borrow=True)
+        self.Wi = theano.shared(self.ortho_weight(256)[:200,:],borrow=True)
+        self.Wf = theano.shared(self.ortho_weight(256)[:200,:],borrow=True)
+        self.Wc = theano.shared(self.ortho_weight(256)[:200,:],borrow=True)
+        self.Wo = theano.shared(self.ortho_weight(256)[:200,:],borrow=True)
+        self.Ui = theano.shared(self.ortho_weight(256),borrow=True)
+        self.Uf = theano.shared(self.ortho_weight(256),borrow=True)
+        self.Uc = theano.shared(self.ortho_weight(256),borrow=True)
+        self.Uo = theano.shared(self.ortho_weight(256),borrow=True)
+        self.bi = theano.shared(np.asarray(np.zeros(256),dtype=theano.config.floatX),borrow=True)
+        self.bf = theano.shared(np.asarray(np.zeros(256),dtype=theano.config.floatX),borrow=True)
+        self.bc = theano.shared(np.asarray(np.zeros(256),dtype=theano.config.floatX),borrow=True)
+        self.bo = theano.shared(np.asarray(np.zeros(256),dtype=theano.config.floatX),borrow=True)
+        self.C0 = theano.shared(np.asarray(np.zeros(256),dtype=theano.config.floatX),borrow=True)
+        self.h0 = theano.shared(np.asarray(np.zeros(256),dtype=theano.config.floatX),borrow=True)
+        self.W2 = theano.shared(np.random.randn(256,vocab_size).astype(theano.config.floatX)*np.sqrt(1.5/vocab_size),borrow=True)
         self.b2 = theano.shared(np.asarray(np.zeros(vocab_size),dtype=theano.config.floatX),borrow=True)
         self.target = T.matrix()
         
+        #nn functions
         self.params = [self.Wi,self.Wf,self.Wc,self.Wo,self.Ui,self.Uf,self.Uc,self.Uo,self.bi,self.bf,self.bc,self.bo,self.h0,self.C0,self.W2,self.b2]
         [self.c,self.h_output],_ = theano.scan(fn=self.step,sequences=self.input,outputs_info=[self.C0,self.h0],non_sequences=self.params[:-4])
         self.output =  T.nnet.softmax(T.dot(self.h_output,self.W2)+self.b2)[20:,:]
@@ -74,6 +82,9 @@ class lstm(object):
         self.predict = theano.function([self.input],self.output,allow_input_downcast=True)
 
     def step(self,input,h0,C0,Wi,Wf,Wc,Wo,Ui,Uf,Uc,Uo,bi,bf,bc,bo):
+        '''
+        lstm memory cell functions
+        '''
         i = T.nnet.sigmoid(T.dot(input,Wi)+T.dot(h0,Ui)+bi)
         cand = T.tanh(T.dot(input,Wc)+T.dot(h0,Uc)+bc)
         f = T.nnet.sigmoid(T.dot(input,Wf)+T.dot(h0,Uf)+bf)
@@ -83,12 +94,18 @@ class lstm(object):
         return c,h
 
     def ortho_weight(self,ndim):
+        '''
+        orthogonal weight initialiation
+        '''
         bound = np.sqrt(1./ndim)
         W = np.random.randn(ndim, ndim)*bound
         u, s, v = np.linalg.svd(W)
         return u.astype(theano.config.floatX)
 
     def adam(self, cost, params, lr=0.0002, b1=0.1, b2=0.01, e=1e-8):
+        '''
+        adam gradient descent updates
+        '''
         updates = []
         grads = T.grad(cost, params)
         self.i = theano.shared(np.float32(0.))
@@ -109,9 +126,16 @@ class lstm(object):
         updates.append((self.i, i_t))
         return updates
 
-#initialize lstm
-print 'initializing lstm'
-NN = lstm(len(w2v.vocab))
+#load saved lstm if it exists, else initialize new lstm
+try:
+    with open("nn.dat","rb") as pickle_nn:
+        NN = pickle.load(pickle_nn)
+    print "saved lstm loaded"        
+except:
+    print "saved lstm not found, initializing new lstm"
+    NN = lstm(len(w2v.vocab))
+    with open("nn.dat","wb") as f:
+        pickle.dump(NN,f,pickle.HIGHEST_PROTOCOL)
 
 #window of words to train over per step
 batch = 120
@@ -123,7 +147,7 @@ for i in range(1000000):
     start = np.random.randint(20,len(dataset)-batch-1)
     
     #intialize word2vec arrays
-    X_train = np.empty((batch,100))
+    X_train = np.empty((batch,200))
     y_train = np.zeros((batch-20,len(w2v.vocab)))
     
     #fill in arrays with word2vec embeddings
@@ -144,7 +168,7 @@ for i in range(1000000):
         
         #use random section of text to initialize
         start = np.random.randint(0,len(dataset)-21)
-        X_test = np.empty((21,100))
+        X_test = np.empty((21,200))
         
         #fill in array with word2vec embeddings
         for j in range(21):
@@ -165,3 +189,7 @@ for i in range(1000000):
             out = NN.predict(X_test)[0]
         
         print string
+        
+        #save neural_net
+        with open("nn.dat","wb") as f:
+            pickle.dump(NN,f,pickle.HIGHEST_PROTOCOL)
