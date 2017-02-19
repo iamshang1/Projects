56e0b93b3299ab59fdf1458497b724a2410e32f0
iamshang1 <emailshang@gmail.com>
2017-02-19 00:05:48 -0500

    refactored inefficient numpy concat into list append

--- Distributed_ML/Image_Recognition/preprocessing.py
+++ Distributed_ML/Image_Recognition/preprocessing.py
@@ -30,25 +30,16 @@ for k in range(2):
         filenames = f.readlines()
 
     inputs = []
-        
-    input = np.empty((0,3,32,32))
     for i in xrange(len(filenames)):
-        if i % 1000 == 0:
-            inputs.append(input)
-            input = np.empty((0,3,32,32))
         print 'Loading file %i of %i:' % (i+1,len(filenames))
         file = filenames[i].replace('\n','').strip()
         image = Image.open("./data/images/%s.png" % file)
         img = np.array(image,dtype='float64')/256
-        img = img.transpose(2, 0, 1).reshape(1,3,32,32)
-        input = np.concatenate((input,img),axis=0)
+        img = img.transpose(2, 0, 1)
+        inputs.append(img)
         image.close()
-    inputs.append(input)
-
-    final = np.empty((0,3,32,32))
-    for i in xrange(len(inputs)):
-        print 'Combining chunks %i of %i:' % (i+1,len(inputs))
-        final = np.concatenate((final,inputs[i]),axis=0)
+    
+    final = np.array(inputs)
     
     if k == 0:
         np.save('X_train', final)
