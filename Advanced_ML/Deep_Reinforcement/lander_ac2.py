0631021c94e14d9aac3ff6900cb1718f20c630df
iamshang1 <emailshang@gmail.com>
2019-03-26 13:28:44 -0400

    cleaned and documented lander_ac2.py

--- Advanced_ML/Deep_Reinforcement/lander_ac2.py
+++ Advanced_ML/Deep_Reinforcement/lander_ac2.py
@@ -6,16 +6,21 @@ from collections import deque
 
 class agent(object):
     def __init__(self,state_size=8,actions=4,gamma=0.99):
-    
-        self.replay_memory = []
+        '''
+        advantage actor critic for lunar lander
+        '''
+        
+        self.episode_memory = []             #store all observations from latest episode
         self.gamma = gamma
         
+        #network inputs
         self.state = tf.placeholder(dtype=tf.float32, shape=[None,state_size])
         self.action = tf.placeholder(dtype=tf.int32, shape=[None])
         self.advantage = tf.placeholder(dtype=tf.float32, shape=[None])
         self.value_target = tf.placeholder(dtype=tf.float32, shape=[None])
         batch_size = tf.reduce_sum(tf.ones_like(self.action))
         
+        #actor network predicts actions for given states
         self.actor_hidden1 = tf.layers.dense(self.state,128,activation=tf.nn.elu,
                         kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.actor_hidden2 = tf.layers.dense(self.actor_hidden1,128,activation=tf.nn.elu,
@@ -24,17 +29,22 @@ class agent(object):
                         kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.action_probs = tf.nn.softmax(self.actor_logits)
 
+        #critic network calculates values of given states
         self.critic_hidden1 = tf.layers.dense(self.state,128,activation=tf.nn.elu,
                         kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.critic_hidden2 = tf.layers.dense(self.critic_hidden1,64,activation=tf.nn.elu,
                         kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.value = tf.layers.dense(self.critic_hidden2,1,
                         kernel_initializer=tf.contrib.layers.xavier_initializer())
-                        
+             
+        #actor loss is calculated using REINFORCE
         neg_log_prob = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=self.actor_logits,labels=self.action)
         self.actor_loss = tf.reduce_mean(neg_log_prob*self.advantage)
+        
+        #critic loss is temporal difference error similar to DQN
         self.critic_loss = tf.reduce_mean(tf.losses.huber_loss(tf.expand_dims(self.value_target,1),self.value))
         
+        #optimizers
         self.optimizer_actor = tf.train.AdamOptimizer(0.001).minimize(self.actor_loss)
         self.optimizer_critic = tf.train.AdamOptimizer(0.0001).minimize(self.critic_loss) 
         
@@ -43,23 +53,31 @@ class agent(object):
         self.sess.run(tf.global_variables_initializer())
     
     def add_memory(self,observation,action,reward,next_observation,done):
+        '''
+        add experience to episode memory
+        '''
     
-        self.replay_memory.append((observation, action, reward, next_observation,done))
+        self.episode_memory.append((observation, action, reward, next_observation,done))
     
     def train(self):
+        '''
+        train model using samples from episode memory
+        '''
     
-        states = np.asarray([elem[0] for elem in self.replay_memory])
-        actions = np.asarray([elem[1] for elem in self.replay_memory])
-        rewards = np.asarray([elem[2] for elem in self.replay_memory])
-        next_states = np.asarray([elem[3] for elem in self.replay_memory])
-        terminal = np.asarray([elem[4] for elem in self.replay_memory])
+        #create batch from episode memory
+        states = np.asarray([elem[0] for elem in self.episode_memory])
+        actions = np.asarray([elem[1] for elem in self.episode_memory])
+        rewards = np.asarray([elem[2] for elem in self.episode_memory])
+        next_states = np.asarray([elem[3] for elem in self.episode_memory])
+        terminal = np.asarray([elem[4] for elem in self.episode_memory])
     
-        #calculate target rewards
+        #calculate value of next states
         d = {self.state:next_states}
         value_next = self.sess.run(self.value,feed_dict=d)
         value_next = np.squeeze(value_next)
         td_targets = rewards + terminal * self.gamma * value_next
         
+        #advantage of given action is next value - current value
         d = {self.state:states}
         value_now = self.sess.run(self.value,feed_dict=d)
         value_now = np.squeeze(value_now)
@@ -69,13 +87,18 @@ class agent(object):
         d = {self.state:states,self.action:actions,self.advantage:td_errors,self.value_target:td_targets}
         actor_loss,critic_loss,_,_ = self.sess.run([self.actor_loss,self.critic_loss,
                                      self.optimizer_actor,self.optimizer_critic],feed_dict=d)
-                    
-        self.replay_memory = []
+        
+        #clear episode memory        
+        self.episode_memory = []
         
         return actor_loss,critic_loss
         
     def predict(self,state):
+        '''
+        predict next action given an input state
+        '''
     
+        #next action is sampled using softmax probabilities assigned to each action
         action_probs = self.sess.run(self.action_probs,feed_dict={self.state:np.expand_dims(state,0)})
         action_probs = np.squeeze(action_probs)
         action = np.random.choice(np.arange(len(action_probs)), p=action_probs)
@@ -83,11 +106,17 @@ class agent(object):
         return action
     
     def save(self,filename):
-
+        '''
+        save network params
+        '''
+        
         self.saver.save(self.sess,filename)
 
     def load(self,filename):
-
+        '''
+        load network params
+        '''
+        
         self.saver.restore(self.sess,filename)
         
 
@@ -96,34 +125,45 @@ env = gym.make('LunarLander-v2')
 state_dim = np.prod(np.array(env.observation_space.shape))
 n_actions = env.action_space.n
 ac2_agent = agent(state_size=state_dim,actions=n_actions)
-#ac2_agent.load("savedmodels/lander.ckpt")
-total_steps = 0
+viz = False
 
+#run episodes
 for ep in range(5000):
     total_reward = 0
     steps_in_ep = 0
     observation = env.reset()
     
+    #run each episode for 1000 steps
     for t in range(1000):
+    
+        #get next action
         action = ac2_agent.predict(observation)
-        
         next_observation, reward, done, _info = env.step(action)
-        #env.render('human')
         total_reward += reward
+        
+        #visualize
+        if viz:
+            env.render('human')
+
+        #normalize reward
         reward /= 100
+        
+        #add obseravtion to memory
         ac2_agent.add_memory(observation, action, reward, next_observation,0.0 if done else 1.0)
         observation = next_observation
         steps_in_ep += 1
-        total_steps += 1
         
+        #terminate episode if done
         if done: 
             break
-            
+    
+    #train network
     actor_loss,critic_loss = ac2_agent.train()
     print('Episode %2i, Reward: %6.2f, Steps: %4i, Actor Loss: %.4f, Critic Loss: %.4f' % 
                 (ep,total_reward,steps_in_ep,actor_loss,critic_loss))
 
-    if ep % 100 == 0:
-        ac2_agent.save("savedmodels/lander.ckpt")
-                
+    #enable vizualization once model gets over 200 score
+    if total_reward >= 200:
+        viz = True
+    
 env.close()
--- Advanced_ML/Deep_Reinforcement/readme.md
+++ Advanced_ML/Deep_Reinforcement/readme.md
@@ -14,19 +14,25 @@ In Q-learning, given a state, each action possible from that state is assigned a
 
 Deep Q-learning networks are generally trained using temporal difference learning. As a result, basic DQNs can be difficult to train because the target Q-values used for training are calculated using the same network that is being trained. In other words, each update made to the network directly effects the calculations of the target Q-values used for the following update,  so the network is always chasing a moving target. One way to mitigate this is to use two networks in parallel -- a fast network and a slow network. The fast network is updated at every step using the target Q-values calculated by the slow network, which is updated once every *X* steps by copying over the parameters from the fast network.
 
+On their own, DQNs will always choose the action with the highest Q-value. If the DQN starts with a suboptimal policy, the DQN may end up consistently choosing suboptimal actions and never explore and learn better actions. Therefore, DQNs generally utilize an explore/exploit schedule -- the network will begin by choosing random actions, and over time the network switches over to choosing actions with the highest Q-value.
+
+An additional common property of DQNs is the use of a replay memory. Because the observations from any one episode are highly correlated, training repeatedly on the most recent observations can cause the DQN to overfit. Therefore, we save the *X* most recent observations into a replay memory and sample randomly from the replay memory to create training batches.
+
 ### Dueling Deep Q-Networks
 
 A potential inefficiency of DQNs is that the network must learn an independent Q-value for each possible action. For problems with large action spaces, this means that the network must explore all possible actions before it will learn the appropriate Q-values for each action. One way to mitigate this is to separate the calculation of the value of each state from the calculation of the relative advantage of each action; the final Q-value is then calculated as the value of the state plus the relative advantage of the selected action. Therefore, the network will know something about the Q-value of an action (based off the value of the state) even if it has not fully explored that action.
 
 ### Policy Networks
 
-Instead of , predict action directly. More stable training and no need for a schedule (required for DQN-based networks); however, policy networks are more likely to get trapped in local optima.
+Instead of trying to calculate the Q-values associated with each action for a given state, we can build a network to predict the best action directly. Given an input state, policy networks generate a softmax probability distribution over all possible actions from that state -- the desired action can then be sampled from that distribution. Compared to DQNs, policy networks are more stable during training, i.e., significant changes in the agent's policy for selection actions are less likely. In addition, because policy networks sample actions from a probability distribution rather than always choosing the action with the highest Q-value, there is no need for an explore/exploit schedule. One downside of policy networks is that they are more likely than DQNs to get trapped in local optima.
 
-Whereas the DQN-based approaches make an update every step in the episode, our policy network trains only once at the end of each episode. In addition, DQN-based approaches often utilize a replay memory in which . On the other hand, our policy network does not utilize a replay memory; instead, each training batch is simply composed of the observations from latest episode.
+Whereas the DQN-based approaches make an update every step in the episode, our policy network trains only once at the end of each episode. In addition, our policy network does not utilize a replay memory; instead, each training batch is simply composed of the observations from latest episode.
 
-### Actor Critic Networks
+### Actor-Critic Networks
 
-like policy networks, trains once per episode
+Policy networks are trained using the REINFORCE algorithm, which pushes the network to choose actions that give the highest future payout. This requires us to know the future payouts associated with each state/action ahead of time. Instead, we can use a neural network to estimate the expected future payouts for a given state and action, similar to in DQNs. An actor-critic network does exactly this using two neural networks -- an actor network and a critic network. The actor network is simply a policy network similar to that described above. The critic network estimates the future payout from a given state, similar to in a DQN, and is used to train the policy network.
+
+Like our policy network, our actor-critic trains once per episode using the observations from that episode. This is inefficient, because the network must wait for a full episode to complete before each update. The training speed of our actor-critic could be greatly improved by utilizing multiple threads (asynchronous actor critic) -- certain threads may be dedicated to running episodes to generate batches of training observations, and the remaining threads can be used to update the network parameters at a faster pace.
 
 ### Notes on Running Code
 
