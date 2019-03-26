9dcbfe8a92f4dc0d7bdeac4e0e82b953c133738d
iamshang1 <emailshang@gmail.com>
2019-03-26 10:22:46 -0400

    cleaned and documented cartpole code

--- Advanced_ML/Deep_Reinforcement/cartpole_dqn.py
+++ Advanced_ML/Deep_Reinforcement/cartpole_dqn.py
@@ -1,16 +1,14 @@
-import os
-#os.environ["CUDA_VISIBLE_DEVICES"]="0"
-
 import numpy as np
 import tensorflow as tf
 import gym
-from skimage import transform 
-from skimage.color import rgb2gray 
 from collections import deque
 import random
 
 class agent(object):
     def __init__(self,action_size=2,gamma=0.95,memory=10000):
+        '''
+        simple deep Q learning network for cartpole
+        '''
     
         #params
         self.explore_start = 1.0            # exploration probability at start
@@ -18,18 +16,19 @@ class agent(object):
         self.decay_rate = 0.001             # exponential decay rate for exploration prob
         self.gamma = gamma
         self.action_size = action_size
-        self.decay_step = 0
+        self.total_steps = 0
     
         #init experience replay memory
-        self.buffer = deque(maxlen=memory)   
+        self.buffer = deque(maxlen=memory)
+        self.burn_in = 2000                 # number of observations before training
     
         #build DQN layers
         self.state = tf.placeholder(tf.float32,shape=(None,4))
         self.action = tf.placeholder(tf.float32,shape=(None,2))
         self.target = tf.placeholder(tf.float32,shape=(None))
-        self.hidden1 = tf.layers.dense(self.state,24,activation=tf.nn.elu,
+        self.hidden1 = tf.layers.dense(self.state,36,activation=tf.nn.elu,
                        kernel_initializer=tf.contrib.layers.xavier_initializer())
-        self.hidden2 = tf.layers.dense(self.hidden1,24,activation=tf.nn.elu,
+        self.hidden2 = tf.layers.dense(self.hidden1,36,activation=tf.nn.elu,
                        kernel_initializer=tf.contrib.layers.xavier_initializer())
         self.output = tf.layers.dense(self.hidden2,2,
                       kernel_initializer=tf.contrib.layers.xavier_initializer())
@@ -45,13 +44,21 @@ class agent(object):
         self.sess = tf.Session()
         self.sess.run(self.init_op)
         
-    def predict(self,stacked_state,explore=True):
+    def predict(self,stacked_state):
+        '''
+        predict next action given an input state
+        '''
+    
+        #calculate explore probability
         r = np.random.rand()
         explore_prob = self.explore_stop + (self.explore_start - self.explore_stop) \
-                       * np.exp(-self.decay_rate * self.decay_step)
+                       * np.exp(-self.decay_rate * self.total_steps)
         
-        if explore and (explore_prob > r):
+        #explore
+        if explore_prob > r:
             action = np.random.randint(0,self.action_size)
+        
+        #exploit
         else:
             d = {self.state:np.expand_dims(stacked_state,0)}
             Qs = self.sess.run(self.output,feed_dict=d)
@@ -59,9 +66,16 @@ class agent(object):
                   
         return action,explore_prob
         
-    def train(self,batch_size=128):
+    def train(self,batch_size=256):
+        '''
+        train model using samples from replay memory
+        '''
     
-        self.decay_step +=1
+        #fill up memory replay before training
+        if len(self.buffer) < self.burn_in:
+            return 0
+    
+        self.total_steps +=1
         
         #get batch
         batch = self.sample_memory(batch_size)
@@ -93,88 +107,76 @@ class agent(object):
         return loss
         
     def add_memory(self,experience):
+        '''
+        add experience to replay memory
+        '''
+        
         self.buffer.append(experience)
     
     def sample_memory(self,batch_size):
+        '''
+        create training batch from replay memory
+        '''
+    
         buffer_size = len(self.buffer)
         idx = np.random.choice(np.arange(buffer_size),size=batch_size,replace=False)
         return [self.buffer[i] for i in idx]
         
     def save(self,filename):
-
         self.saver.save(self.sess,filename)
 
     def load(self,filename):
-
         self.saver.restore(self.sess,filename)
         
-
+#initialize environment
 env = gym.make('CartPole-v1')
 action_size = env.action_space.n
 DQagent = agent(action_size=action_size)
+viz = False
 
-for ep in range(50000):
-    print('training episode %i' % (ep+1))
+#run episodes
+for ep in range(500):
     state = env.reset()
     done = False
     t = 0
     episode_rewards = []
     mean_loss = []
-    
     explore_prob = 1.0
     
+    #terminate episode when done
     while not done and t < 200:
     
         t += 1
-    
-        if len(DQagent.buffer) < 1000:
-            action = env.action_space.sample()
-            new_state, reward, done, info = env.step(action)
-            reward /= 5
-            
-            if done:
-                reward = -1
-            
-            DQagent.add_memory((state,action,reward,new_state,done))
-            state = new_state
 
-        else:
-
-            action,explore_prob = DQagent.predict(state)
-            new_state, reward, done, info = env.step(action)
-            episode_rewards.append(reward)
-            reward /= 5
-
-            if done:
-                reward = -1
-                
-            DQagent.add_memory((state,action,reward,new_state,done))
-            state = new_state
-            mean_loss.append(DQagent.train())
+        #get next action
+        action,explore_prob = DQagent.predict(state)
+        new_state, reward, done, info = env.step(action)
+        episode_rewards.append(reward)
         
-    # Get the total reward of the episode
+        #visualize
+        if viz:
+            env.render('human')
+        
+        #normalize rewards
+        reward /= 5
+        if done:
+            reward = -1
+            
+        #update memory and train
+        DQagent.add_memory((state,action,reward,new_state,done))
+        state = new_state
+        mean_loss.append(DQagent.train())
+        
+    #show episode results
     loss = np.mean(mean_loss)
     total_reward = np.sum(episode_rewards)
     print('Episode: {}'.format(ep+1),
           'Total reward: {}'.format(total_reward),
           'Explore P: {:.4f}'.format(explore_prob),
           'Training Loss {:.4f}'.format(loss))
-                  
-    if ep % 10 == 0:
-        DQagent.save("savedmodels/cartpole.ckpt")
-'''
-#show trained agent
-DQagent.load("savedmodels/cartpole.ckpt")
-for ep in range(10):
-    state = env.reset()
-    done = False
-    t = 0
-    
-    while not done and t < 10000:
-    
-        t += 1
-        action,explore_prob = DQagent.predict(state,explore=False)
-        state, reward, done, info = env.step(action)
-        env.render('human')
-'''
+          
+    #enable vizualization once model gets a perfect score
+    if total_reward == 200:
+        viz = True
+
 env.close()
